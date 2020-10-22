13a035408c9e6afac068e7b993b3bb5abf349c25
zubaira <zubair@dhis2.org>
2020-10-22 21:50:11 +0200

    minor refactor

--- working.js
+++ working.js
@@ -11,7 +11,6 @@ let downloadServerAuth = 'Basic ' + Buffer.from(`${properies.get('download.serve
 let uploadServerAuth = 'Basic ' + Buffer.from(`${properies.get('upload.server.username')}:${properies.get('upload.server.password')}`).toString('base64');
 
 let getOptions = {
-	"method": "GET",
 	"hostname": properies.get('download.server.host'),
 	"path": properies.get('download.server.path'),
 	"headers": {
@@ -22,7 +21,6 @@ let getOptions = {
   };
 
   let postOptions = {
-	"method": "POST",
 	"hostname": properies.get('upload.server.host'),
 	"path": properies.get('upload.server.path'),
 	"headers": {
@@ -32,26 +30,25 @@ let getOptions = {
 	}
   };
 
-
-let downloadOrgUnitData = ( getOptions ) => {
+let downloadOrgUnitData = (getOptions) => {
 
     let data = [];
-    return new Promise( (resolve,reject) => {
-        const req = http.request( getOptions, res => {
+    return new Promise((resolve,reject) => {
+        const req = http.get( getOptions, res => {
             if ( res.statusCode  === 200 ){
                 console.log(`Connected to server: ${getOptions.hostname} with Status code ${res.statusCode}`);
             }
             else{
-                reject( `Connection with server: ${getOptions.hostname} refused with Status code ${res.statusCode}` );
+                reject(`Connection with server: ${getOptions.hostname} refused with Status code ${res.statusCode}`);
             }
             res.on('data', receivedData => {
                 data.push( receivedData );                
             })
-                .on( 'end', () => {
+                .on('end', () => {
                     fileSystem.appendFile( outPutFile, data, function (err) {
                     if (err) return console.error(err);
 
-                    resolve('OrganisationUnit data downloaded in file: ' + outPutFile );
+                    resolve('OrganisationUnit data downloaded in file: ' + outPutFile);
                     }); 
                 })
         });
@@ -59,19 +56,17 @@ let downloadOrgUnitData = ( getOptions ) => {
         req.on('error', error => {
             reject(error);
           });
-          
-        req.end();
     });
 }
 
 let uploadOrgUnitData = ( postOptions ) => {
-    return new Promise( (resolve,reject) => {
-        const req = http.request( getOptions, res => {
-            if ( res.statusCode  === 200 ){
+    return new Promise((resolve,reject) => {
+        const req = http.post( getOptions, res => {
+            if (res.statusCode  === 200){
             console.log(`Connected to server: ${postOptions.hostname} with Status code ${res.statusCode}`);
             }
             else{
-                reject( `Connection with server: ${postOptions.hostname} refused with Status code ${res.statusCode}` );
+                reject(`Connection with server: ${postOptions.hostname} refused with Status code ${res.statusCode}`);
             }
         });
         
@@ -85,22 +80,22 @@ let uploadOrgUnitData = ( postOptions ) => {
 
 function loadProperties()
 {
-    if ( !fileSystem.existsSync( args['file'])){
+    if ( !fileSystem.existsSync(args['file'])){
         console.error('Properties file not found');
         process.exit();
     }
 
     console.log('Loading properties from file');
-    return new propertiesReader( args['file']);
+    return new propertiesReader(args['file']);
 }
 
 let startSynchronization = async () => {
 
-    let downloadResponse = await downloadOrgUnitData( getOptions ).catch( error => console.error( error ) );
-    console.log( downloadResponse );
+    let downloadResponse = await downloadOrgUnitData(getOptions).catch(error => console.error(error));
+    console.log(downloadResponse);
 
-    let uploadResponse = await uploadOrgUnitData( postOptions ).catch( error => console.error( error));
-    console.log( uploadResponse );
+    let uploadResponse = await uploadOrgUnitData(postOptions).catch(error => console.error(error));
+    console.log(uploadResponse);
 }
 
 startSynchronization();
